0ab16af52c04e95e856e6038fbe703c2babcb44f
Rosario <marcodibartolo2@gmail.com>
2024-03-17 17:02:22 +0100

    3.0

--- src/Pages/Tasks.tsx
+++ src/Pages/Tasks.tsx
@@ -1,10 +1,11 @@
 // Import necessary libraries
-import { useEffect, useMemo, useState } from "react";
+import { useMemo,   useState } from "react";
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -13,10 +14,15 @@ import FileUploader from "../FileUploader";
 import { Button } from "../components/ui/button";
 import { saveAs } from "file-saver";
 import { backend } from "@/config";
-import { AuthStatusInfo } from "@/lib/AuthStatus";
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
-import { MinusCircleIcon, MinusIcon, XIcon } from "lucide-react";
-
+import { XIcon } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 type value = {
   Notte: number;
   Mattina: number;
@@ -38,50 +44,57 @@ interface yearRowProps {
   Delete: () => void;
 }
 
+type StateInfo<T> = {
+  message: string | object;
+  type: T;
+};
+
+type AnalysisState = "default" | "loading" | "success" | "error";
+
+type AnalysisStateInfo = StateInfo<AnalysisState>;
+
 function Year({ year, values, Delete }: yearRowProps) {
   return (
-    <TableRow className="border bg-slate-600 marker:">
-      <TableCell>{year}</TableCell>
-      <TableCell>{values.Mattina}</TableCell>
-      <TableCell>{values.Pomeriggio}</TableCell>
-      <TableCell>{values.Notte}</TableCell>
-      <TableCell>
-        <Button onClick={Delete}  >
-          Elimina  
-        </Button>
+    <TableRow className="border   marker:">
+      <TableCell className="p-2">{year}</TableCell>
+      <TableCell className="p-2">{values.Mattina}</TableCell>
+      <TableCell className="p-2">{values.Pomeriggio}</TableCell>
+      <TableCell className="p-2">{values.Notte}</TableCell>
+      <TableCell className="p-2">
+        <Button onClick={Delete}>Elimina</Button>
       </TableCell>
     </TableRow>
   );
 }
-
+type Azienda = "policlinico" | "pisa"
 function Block({ Block }: { Block: File }) {
+  const [Azienda, setAzienda] = useState<Azienda>( )
+ 
   const [BlockState, setBlockState] = useState<AnalysisData>({
     Nome: "",
     Values: {},
   });
-  const [Status, setStatus] = useState<AuthStatusInfo>({
-    type: "loading",
+
+  const [Status, setStatus] = useState<AnalysisStateInfo>({
+    type: "default",
     message: "",
   });
 
-  console.log(Status);
+  const analyze = () => {
+    const formData = new FormData();
+    formData.append(Block.name, Block);
 
-  useEffect(() => {
-    const analyze = async () => {
-      const formData = new FormData();
-      console.log("Waiting for a response...");
+    const response = backend.path(`/analyze/${Azienda}`).post(formData, {
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Set the appropriate origin or '*' for any origin
+        credentials: "same-origin",
+      },
+    });
 
-      formData.append(Block.name, Block);
+    console.log("Waiting for a response...");
+    setStatus((prev) => ({ ...prev, type: "loading" }));
 
-      return await backend.path("/analyze").post(formData, {
-        headers: {
-          "Access-Control-Allow-Origin": "*", // Set the appropriate origin or '*' for any origin
-          credentials: "same-origin",
-        },
-      });
-    };
-
-    analyze()
+    response
       .then((response) => {
         console.log("Request finished");
 
@@ -94,7 +107,9 @@ function Block({ Block }: { Block: File }) {
       .catch((error) => {
         setStatus({ type: "error", message: error as string });
       });
-  }, []);
+  };
+
+  console.log(Status);
 
   const Totale = useMemo(() => {
     let Mattine = 0;
@@ -110,32 +125,52 @@ function Block({ Block }: { Block: File }) {
     return { Mattine, Pomeriggi, Notti };
   }, [BlockState]);
 
-  console.log(BlockState);
+  const ConversionTable: Record<AnalysisState, React.ReactNode> = {
+    default: "",
+    success: (
+      <Button
+        onClick={() => {
+          console.log(BlockState)
+          const nome = BlockState.Nome;
+          if (BlockState) {
+            console.log(BlockState);
+            Object.entries(BlockState.Values).forEach(([k, v]) => {
+              const content = `${nome}\nMattine:  ${v.Mattina}\nPomeriggi:  ${v.Pomeriggio}\nNotti:  ${v.Notte}`;
+              const blob = new Blob([content], { type: "text/plain" });
+              saveAs(blob, `${nome}-${k}.txt`);
+            });
+          }
+        }}
+        className="bg-green-700 text-white  focus:outline-none"
+      >
+        Scarica i risultati
+      </Button>
+    ),
+    loading: <LoadingSpinner  className="w-full"/>,
 
-  console.log(Totale);
-
+    error: (
+      <span className="flex">
+        Errore nel processare i dati... <XIcon className="text-red-700" />{" "}
+      </span>
+    ),
+  };
   return (
-    <div className="m-3 overflow-hidden bg-slate-600 border-b-0 shadow-lg shadow-slate-400  text-white border-muted border-2">
-      <Table className="overflow-hidden">
-        <TableCaption className="text-nowrap py-5 hover:text-white">
-          Conteggi di timbrature (entrata e uscita) per{" "}
-          <span className="text-yellow-500">
-            {" "}
-            {BlockState.Nome || Block.name}{" "}
-          </span>
-        </TableCaption>
-        <TableHeader className="bg-slate-500   w-full">
-          <TableRow>
-            <TableHead className="text-white  text-center ">Anno</TableHead>{" "}
-            <TableHead className="text-white  text-center ">Mattine</TableHead>{" "}
+    <div className="m-3 overflow-hidden   border-b-0 shadow-lg shadow-slate-400  text-white border-muted border-2">
+      <Table className="overflow-hidden bg-slate-800 ">
+        <TableCaption className="text-nowrap absolute hover:text-white"></TableCaption>
+        <TableHeader className=" w-full">
+          <TableRow className=" ">
+            <TableHead className="text-white  text-center ">Anno</TableHead>
+            <TableHead className="text-white  text-center ">Mattine</TableHead>
             <TableHead className="text-white  text-center ">
-              Pomeriggi
-            </TableHead>{" "}
+              Pomeriggi{" "}
+            </TableHead>
             <TableHead className="text-white  text-center ">Notti</TableHead>
+            <TableHead className="text-white  text-center ">Azione</TableHead>
           </TableRow>
         </TableHeader>
 
-        <TableBody className="overflow-hidden bg-gradient-to-t from-slate-300 rounded-sm  shadow-slate-400 ">
+        <TableBody className="overflow-hidden bg-slate-500 bg-gradient-to-t from-slate-700 rounded-sm   ">
           {Object.entries(BlockState?.Values).map(([year, values]) => (
             <Year
               key={year}
@@ -143,56 +178,58 @@ function Block({ Block }: { Block: File }) {
               values={values}
               Delete={() => {
                 // Create a new object without the deleted year
-                 
-                setBlockState(
-                  (prev) => {
 
-                  const newValues = { ...(prev.Values) };
-                  delete newValues[ year ];
-                  return   { ...prev, Values: newValues }
-                
-                
-                 
-                })
+                setBlockState((prev) => {
+                  const newValues = { ...prev.Values };
+                  delete newValues[year];
+                  return { ...prev, Values: newValues };
+                });
               }}
             />
           ))}
+          <TableRow  >
+            <TableCell colSpan={1} className=" "> Totale:</TableCell>
+            {Object.entries(Totale).map(([k, v]) => (
+              <TableCell className=" " key={k}>
+                {k}: {v}
+              </TableCell>
+            ))}
+            <TableCell>{ConversionTable[Status.type]}</TableCell>
+          </TableRow>
         </TableBody>
-      </Table>
-      <div className="flex items-center px-3 justify-between w-full bg-slate-800 text-white">
-        <div className="flex">
-          <TableCell> Totale:</TableCell>{" "}
-          {Object.entries(Totale).map(([k, v]) => (
-            <TableCell key={k}>
-              {k}: {v}{" "}
+
+        <TableFooter className="bg-slate-800 hover:text-slate-900">
+           <TableRow>
+            <TableCell  >
+              <Button
+                className="bg-slate-500 bg-gradient-to-t from-slate-700"
+                onClick={analyze}
+              >
+                Richiedi il Conteggio...
+              </Button>
             </TableCell>
-          ))}
-        </div>
-        {Status.type === "success" ? (
-          <Button
-            onClick={() => {
-              const nome = BlockState?.Nome;
-              if (BlockState) {
-                console.log(BlockState);
-                Object.entries(BlockState.Values).forEach(([k, v]) => {
-                  const content = `${nome}\nMattine:  ${v.Mattina}\nPomeriggi:  ${v.Pomeriggio}\nNotti:  ${v.Notte}`;
-                  const blob = new Blob([content], { type: "text/plain" });
-                  saveAs(blob, `${nome}-${k}.txt`);
-                });
-              }
-            }}
-            className="bg-green-700 text-white  focus:outline-none"
-          >
-            Scarica i risultati
-          </Button>
-        ) : Status.type == "loading" ? (
-          <LoadingSpinner />
-        ) : (
-          <span className="flex">
-            Errore nel processare i dati... <XIcon className="text-red-700" />{" "}
-          </span>
-        )}
-      </div>
+
+            <TableCell colSpan={3} className="">
+              Conteggi di timbrature (entrata e uscita) per
+              <span className="text-yellow-500 mx-1">
+                {BlockState.Nome || Block.name}
+              </span>
+            </TableCell>
+            <TableCell>
+              <Select onValueChange={(value) => setAzienda(value as Azienda)} >
+                <SelectTrigger className="w-[180px] bg-slate-500 bg-gradient-to-t from-slate-700">
+                  <SelectValue placeholder="Azienda" />
+                </SelectTrigger>
+                <SelectContent className="bg-slate-500 bg-gradient-to-t from-slate-700 text-white">
+                  <SelectItem value="Policlinico">Policlinico</SelectItem>
+                  <SelectItem value="Pisa">Pisa</SelectItem>
+                </SelectContent>
+              </Select>
+            </TableCell>
+          </TableRow>
+            
+        </TableFooter>
+      </Table>
     </div>
   );
 }
