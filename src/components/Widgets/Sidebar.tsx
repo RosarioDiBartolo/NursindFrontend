b13aaa15b74d249a6ef3270315e315c93e7d8482
Rosario <marcodibartolo2@gmail.com>
2024-03-19 10:22:54 +0100

    patch 3.0.1

--- src/components/Widgets/Sidebar.tsx
+++ src/components/Widgets/Sidebar.tsx
@@ -1,10 +1,23 @@
-import { Settings  } from "lucide-react";
+import { Settings,   UserCircle   } from "lucide-react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "../ui/button";
+import { signOut } from "firebase/auth";
+
+function UserAccordition() {
+
+  return (
+    <AccordionItem value="User">
+      <AccordionTrigger className="justify-start gap-3"><UserCircle className='no-rot' /> <span>Utente</span>       </AccordionTrigger>
+      <AccordionContent className="bg-slate-100 rounded-md p-3"><Button onClick={signOut} variant="link" className='hover:text-red-700 deco'  >Logout</Button>  </AccordionContent>
+    </AccordionItem>
+  );
+}
+
 function Sidebar() {
   return (
     <div className="border bg-white p-3 w-[400px] text-start shadow-md shadow-slate-600">
@@ -16,27 +29,16 @@ function Sidebar() {
 
       <div>
         <Accordion type="single" collapsible>
-          <AccordionItem value="item-1" >
-            <AccordionTrigger>
-                Utente Is it accessible? 
-            </AccordionTrigger>
-            <AccordionContent className='bg-slate-100 rounded-md p-3'>
+          <UserAccordition />
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Utente Is it accessible?</AccordionTrigger>
+            <AccordionContent className="bg-slate-100 rounded-md p-3">
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-2" >
-            <AccordionTrigger>
-                Utente Is it accessible? 
-            </AccordionTrigger>
-            <AccordionContent className='bg-slate-100 rounded-md p-3'>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3" >
-            <AccordionTrigger>
-                Utente Is it accessible? 
-            </AccordionTrigger>
-            <AccordionContent className='bg-slate-100 rounded-md p-3'>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Utente Is it accessible?</AccordionTrigger>
+            <AccordionContent className="bg-slate-100 rounded-md p-3">
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
